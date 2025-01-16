2cca6e162292280741fa26bdaae7635540db26d9
noe <60700919+eNiiju@users.noreply.github.com>
2025-01-16 11:03:03 +0100

    wip: new map

--- web/src/lib/components/Map.svelte
+++ web/src/lib/components/Map.svelte
@@ -1,37 +1,9 @@
-<script lang="ts">
-	let L: typeof import('leaflet');
-	let map: L.Map;
-
-	async function importLeaflet() {
-		L = await import('leaflet');
-	}
-
-	async function initMap(element: HTMLElement) {
-		await importLeaflet();
-
-		map = L.map(element).setView([51.505, -0.09], 13);
-
-		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution:
-				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
-
-		// L.marker([51.5, -0.09])
-		// 	.addTo(map)
-		// 	.bindPopup('A pretty CSS popup.<br> Easily customizable.')
-		// 	.openPopup();
-	}
-
-	function mapAction(element: HTMLElement) {
-		$effect(() => {
-			initMap(element);
-
-			return () => {
-				map.remove();
-			};
-		});
-	}
+<!-- <script lang="ts">
+	import { MapLibre } from 'svelte-maplibre';
 </script>
 
-<!-- Map -->
-<div use:mapAction class="size-full"></div>
+<MapLibre
+	style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+	class="relative aspect-[9/16] max-h-[70vh] w-full sm:aspect-video sm:max-h-full"
+	standardControls
+/> -->
