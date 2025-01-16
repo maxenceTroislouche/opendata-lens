c2fae3d82424dc6dc0e463ffac18127463e5f009
noe <60700919+eNiiju@users.noreply.github.com>
2025-01-16 11:34:50 +0100

    feat: add new map

--- web/src/lib/components/Map.svelte
+++ web/src/lib/components/Map.svelte
@@ -1,9 +1,14 @@
-<!-- <script lang="ts">
+<script lang="ts">
 	import { MapLibre } from 'svelte-maplibre';
+
+	const defaultLattitude = 50.432649;
+	const defaultLongitude = 2.81951;
 </script>
 
 <MapLibre
+	center={[defaultLongitude, defaultLattitude]}
+	zoom={15}
+	standardControls
 	style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
 	class="relative aspect-[9/16] max-h-[70vh] w-full sm:aspect-video sm:max-h-full"
-	standardControls
-/> -->
+/>
