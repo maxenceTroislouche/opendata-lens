096d9ce0b8f1054d00b4e238e7fcc5ec7fdbb816
Paul-Colin <paul.colin@ig2i.centralelille.fr>
2025-01-20 11:09:12 +0100

    conversion coordonnées

--- tasks/commerces/hotel.py
+++ tasks/commerces/hotel.py
@@ -1,15 +1,34 @@
 import pandas as pd
+import json
+from pymongo import MongoClient
+from pyproj import Transformer
 
+# Configuration de la connexion MongoDB
+MONGO_URI = "mongodb://admin:securepassword@localhost:27017/"
+DB_NAME = "opendatalens"
+COLLECTION_NAMES = ("commerce", "evenement")
+
+# Connexion au serveur MongoDB
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+# lien vers l'open data : https://opendata.agglo-lenslievin.fr/datasets/e1c55f7ec4d049a7a04488a0694e6d82_0/explore?location=50.439939%2C2.815297%2C14.74
 # Charger le fichier CSV pour en examiner le contenu
 input_file = "data/HA9bergements_touristiques.csv"
 data = pd.read_csv(input_file)
 
-import pandas as pd
 
 output_file = "formatted_hotel.csv"
 
 # Charger les données
 data = pd.read_csv(input_file)
+
+# Convertir x et y map en longitude et latitude 
+# Définir le transformateur pour la conversion Lambert 93 (EPSG:2154) -> WGS84 (EPSG:4326)
+transformer = Transformer.from_crs("EPSG:3950", "EPSG:4326", always_xy=True)
+
+# Effectuer la conversion des coordonnées
+data['Longitude'], data['Latitude'] = transformer.transform(data['X'], data['Y'])
 
 # Reformater les données
 formatted_data = pd.DataFrame({
@@ -17,12 +36,16 @@ formatted_data = pd.DataFrame({
     "Adresse": data["adresse"] + ", " + data["ville"] + " " + data["cp"].astype(str),
     "Mail": data["e_mail"],  # Pas d'information sur les emails dans le fichier
     "Type": "Hébergement",  # Statique pour ce fichier
-    "Longitude": data["X"],
-    "Latitude": data["Y"]
+    "Longitude": data["Longitude"],
+    "Latitude": data["Latitude"]
 })
 
 # Exporter les données formatées dans un nouveau fichier CSV
 formatted_data.to_csv(output_file, index=False, sep=";")
+# Enregistrement des données formatées dans la base de données
+collection = COLLECTION_NAMES[0]
+print(f"Enregistrement des données dans {collection}")
+db[collection].insert_many(formatted_data.to_dict(orient="records"))
 
 print(f"Données formatées et enregistrées dans {output_file}")
 
